df10233045118ce065603261f2e37b87b28050d9
luckalway <luckalway@126.com>
2018-04-20 09:53:20 +0800

    refactor home page.

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var client = require('../services/client');
-var colors = ['Orange','Yellow','Olive','Green','Teal','Blue','Violet','Purple','Pink','Brown','Grey','Black'];
+var colors = ['Orange', 'Yellow', 'Olive', 'Green', 'Teal', 'Blue', 'Violet', 'Purple', 'Pink', 'Brown', 'Grey', 'Black'];
 
 
 router.get('/', function(req, res) {
-  client.get('/messages?descending=true', function(err, messages){
-    if(err){
+  client.get('/messages?descending=true', function(err, messages) {
+    if (err) {
       return next(err);
     }
-    var i=0;
-    messages.forEach(function(item){
-      item.color = colors[i%colors.length].toLowerCase();
-      item.title = item.title.replace('【讲台】','');
+    var i = 0;
+    messages.forEach(function(item) {
+      item.color = colors[i % colors.length].toLowerCase();
+      item.title = item.title.replace('【讲台】', '');
       i++;
     });
 
     res.render('home', {
-      messages:messages
+      messages: messages
     });
-  })
+  });
+});
 
+router.get('/test', function(req, res) {
+  res.render('test', {});
 });
 
 module.exports = router;
